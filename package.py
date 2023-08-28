a2a27b7ea5f5d4aac63d14bceefca19d4a5f62f3
Kern Attila GERMAIN <5556461+KernAttila@users.noreply.github.com>
2023-08-29 01:13:23 +0200

    feat: add package.py for rez

---
+++ package.py
@@ -0,0 +1,14 @@
+name = "cueNimby"
+
+version = "0.1.0"
+
+description = "User managed availability scheduler for OpenCue"
+
+authors = ["Kern Attila Germain"]
+
+requires = ["PySide2"]
+
+cachable = False
+
+def commands():
+    env.PYTHONPATH.append("{root}")
